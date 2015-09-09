3dcce79513c01b5389ae395ef4534d3a39fa7383
jaroot32 <jonathanroot15@gmail.com>
2015-09-08 22:39:01 -0700

    fix: added media queries to the layout

--- js/main.js
+++ js/main.js
@@ -14,9 +14,7 @@ $(document).ready(function() {
  
     //"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
     //The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    
     overlay               :   false,
-    
     overlayClass          :   'animsition-overlay-slide',
     overlayParentElement  :   'body'
   });
