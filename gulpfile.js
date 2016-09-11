0ee936eb4126d66ffef4177c87feba5b5bf5eace
jaroot32 <jonathanroot15@gmail.com>
2016-09-10 18:55:00 -0700

    hover effect for images

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var shell = require('gulp-shell');
+var browserSync = require('browser-sync').create();
 
 gulp.task("concatScripts", function(){
 	gulp.src(['bower_components/jquery/dist/jquery.min.js', 'js/instafeed.min.js', 'js/jquery.animsition.js', 'js/main.js'])
@@ -16,3 +18,16 @@ gulp.task("minifyScripts", function () {
 		.pipe(gulp.dest('js'));
 });
 
+// Task for building blog when something changed:
+gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
+// Or if you don't use bundle:
+gulp.task('build', shell.task(['jekyll build --watch']));
+
+// Task for serving blog with Browsersync
+gulp.task('serve', function () {
+    browserSync.init({server: {baseDir: '_site/'}});
+    // Reloads page when some of the already built files changed:
+    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['build', 'serve']);
